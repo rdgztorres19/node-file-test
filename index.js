6bd858b7b41972ebf6dca1578d5124bdbbcc7690
Manuel Rodriguez <mrodriguez@sorba.ai>
2023-02-22 10:20:46 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs-extra');
 
-const originPath = "/tmp/aa.txt";
-const destinationPath = "/tmp/aa1.txt";
+const originPath = "/srv/sorba_devices/carbon.super/d0c2e200-b21a-11ed-8ef6-3b7647862b6f/files/sql.sql";
+const destinationPath = "/srv/sorba_devices/carbon.super/6c8da630-b2c4-11ed-b7ca-795edfaf7fd0/files/sql.sql";
 
 fs.copy(originPath, destinationPath, { overwrite: true })
     .then(() => console.log('success!'))
