23bb3f47b241e8e62e05d0d96a0c8582374b6fc5
Manuel Rodriguez <mrodriguez@sorba.ai>
2023-02-22 10:22:43 -0500

    update

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const fs = require('fs-extra');
 
-const originPath = "/srv/sorba_devices/carbon.super/d0c2e200-b21a-11ed-8ef6-3b7647862b6f/files/sql.sql";
+const originPath = "/tmp/aa.txt";
 const destinationPath = "/srv/sorba_devices/carbon.super/6c8da630-b2c4-11ed-b7ca-795edfaf7fd0/files/sql.sql";
 
 fs.copy(originPath, destinationPath, { overwrite: true })
     .then(() => console.log('success!'))
     .catch(err => console.error(err))
+
+//export PATH=$PATH:/layers/heroku_nodejs-engine/dist/bin/:/layers/heroku_nodejs-npm/npm/bin/
