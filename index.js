0dc1d01ae45b4e2c360e5aeab64f842b60b71352
Manuel Rodriguez <mrodriguez@sorba.ai>
2023-02-22 10:26:25 -0500

    update

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const fs = require('fs-extra');
+const fsNative = require('fs');
 
 const originPath = "/tmp/aa.txt";
 const destinationPath = "/srv/aa.txt";
 
-fs.copy(originPath, destinationPath, { overwrite: true })
+try {
+    fsNative.copyFileSync(originPath, destinationPath, fsNative.constants.COPYFILE_EXCL);
+} catch (error) {
+    console.log(error);
+}
+
+
+/*fs.copy(originPath, destinationPath, { overwrite: true })
     .then(() => console.log('success!'))
-    .catch(err => console.error(err))
+    .catch(err => console.error(err))*/
 
 //export PATH=$PATH:/layers/heroku_nodejs-engine/dist/bin/:/layers/heroku_nodejs-npm/npm/bin/
